35356a3301a35fcdd534567becee4eb5466cfd1b
Ludovic Blervaque Brient <ludovic.blervaque@gmail.com>
2025-08-02 20:06:12 +0900

    Version 1.0 release

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repoName = 'zonecropper-ai'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
   optimizeDeps: {
     include: ['pdfjs-dist'],
   },
